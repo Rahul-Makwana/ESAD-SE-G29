5412a27be5878c28a130ad5b21fee5e8711229c6
Rahul-Makwana <rahulamakwana20@gnu.ac.in>
2022-11-09 14:36:12 +0530

    update

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost:3036');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', 'root');
+   define('DB_DATABASE', 'ESAD_SE');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
---
+++ logout.php
@@ -0,0 +1,15 @@
+<?php
+echo 'Hello World';
+?>
+<html">
+   
+   <head>
+      <title>Welcome </title>
+   </head>
+   
+   <body>
+      <h1>Welcome <?php echo $login_session; ?></h1> 
+      <h2><a href = "logout.php">Sign Out</a></h2>
+   </body>
+   
+</html
